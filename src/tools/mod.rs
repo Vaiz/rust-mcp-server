73c14f9f2b9ba490a1f525a03974ed724f14eab8
Vaiz <4908982+Vaiz@users.noreply.github.com>
2025-06-24 13:06:02 +0200

    add annotations

--- src/tools/mod.rs
+++ src/tools/mod.rs
@@ -2,7 +2,7 @@ pub mod cargo;
 pub mod test;
 
 use rust_mcp_sdk::schema::{
-    CallToolResult, CallToolResultContentItem, schema_utils::CallToolError,
+    schema_utils::CallToolError, Annotations, CallToolResult, CallToolResultContentItem, Role
 };
 
 use test::{SayHelloTool, SayGoodbyeTool};
@@ -12,13 +12,24 @@ fn execute_command(mut cmd: std::process::Command) -> Result<CallToolResult, Cal
     let output = cmd.output();
     match output {
         Ok(output) => {
-            let stdout = String::from_utf8_lossy(&output.stdout);
-            let stderr = String::from_utf8_lossy(&output.stderr);
+            let stdout = String::from_utf8_lossy(&output.stdout.trim_ascii());
+            let stderr = String::from_utf8_lossy(&output.stderr.trim_ascii());
 
-            let content = vec![
-                CallToolResultContentItem::text_content(stdout.to_string(), None),
-                CallToolResultContentItem::text_content(stderr.to_string(), None),
-            ];
+            let mut content = Vec::new();
+            if !stdout.is_empty() {
+                let annotations = Some(Annotations{
+                    audience: vec![Role::User, Role::Assistant],
+                    priority: Some(0.1)
+                });
+                content.push(CallToolResultContentItem::text_content(stdout.into(), annotations));
+            }
+            if !stderr.is_empty() {
+                let annotations = Some(Annotations{
+                    audience: vec![Role::User, Role::Assistant],
+                    priority: Some(1.)
+                });
+                content.push(CallToolResultContentItem::text_content(stderr.into(), annotations));
+            }
             Ok(CallToolResult {
                 content,
                 is_error: Some(!output.status.success()),
