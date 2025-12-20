ca391bd098663c9c56d3dbf82e577959157608f0
Vaiz <4908982+Vaiz@users.noreply.github.com>
2025-12-20 16:54:53 +0100

    add tests

--- src/response.rs
+++ src/response.rs
@@ -42,3 +42,110 @@ impl From<Output> for Response {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use rmcp::model::{AnnotateAble, Annotations, RawContent};
+
+    use crate::command::{CommandLine, ExitStatus, Stdout};
+
+    use super::*;
+
+    #[test]
+    fn only_output() {
+        let output = Output {
+            tool_name: "test_tool".into(),
+            stdout: Some(Stdout("This is a test output".into())),
+            stderr: None,
+            cmd_line: CommandLine("test_command --option".into()),
+            exit_status: ExitStatus(std::process::ExitStatus::default()),
+        };
+        let response: Response = output.into();
+        let rmcp_result = response.into_rmcp_result(false);
+
+        let [cmd_line, stdout, exit_status] = &rmcp_result.content[..] else {
+            panic!("expected 3 content items: {rmcp_result:?}");
+        };
+
+        assert_eq!(
+            cmd_line.as_text().unwrap().text,
+            "Executed command: `test_command --option`"
+        );
+        assert_eq!(stdout.as_text().unwrap().text, "This is a test output");
+        assert_eq!(exit_status.as_text().unwrap().text, "✅ test_tool: Success");
+    }
+
+    #[test]
+    fn with_additional_content_and_recommendations() {
+        let output = Output {
+            tool_name: "test_tool".into(),
+            stdout: Some(Stdout("This is a test output".into())),
+            stderr: None,
+            cmd_line: CommandLine("test_command --option".into()),
+            exit_status: ExitStatus(std::process::ExitStatus::default()),
+        };
+        let mut response: Response = output.into();
+        response.add_content(
+            RawContent::text("additional content").annotate(Annotations {
+                audience: None,
+                priority: None,
+                last_modified: None,
+            }),
+        );
+        response.add_recommendation("Consider checking the logs.");
+
+        let rmcp_result = response.into_rmcp_result(false);
+
+        let [
+            cmd_line,
+            stdout,
+            exit_status,
+            additional_content,
+            recommendation,
+        ] = &rmcp_result.content[..]
+        else {
+            panic!("expected 5 content items: {rmcp_result:?}");
+        };
+
+        assert_eq!(
+            cmd_line.as_text().unwrap().text,
+            "Executed command: `test_command --option`"
+        );
+        assert_eq!(stdout.as_text().unwrap().text, "This is a test output");
+        assert_eq!(exit_status.as_text().unwrap().text, "✅ test_tool: Success");
+        assert_eq!(
+            additional_content.as_text().unwrap().text,
+            "additional content"
+        );
+        assert_eq!(
+            recommendation.as_text().unwrap().text,
+            "RECOMMENDATION: Consider checking the logs."
+        );
+    }
+
+    #[test]
+    fn ignore_recommendations() {
+        let output = Output {
+            tool_name: "test_tool".into(),
+            stdout: Some(Stdout("This is a test output".into())),
+            stderr: None,
+            cmd_line: CommandLine("test_command --option".into()),
+            exit_status: ExitStatus(std::process::ExitStatus::default()),
+        };
+        let mut response: Response = output.into();
+        response.add_recommendation("Consider checking the logs.");
+
+        let rmcp_result = response.into_rmcp_result(true);
+
+        let [cmd_line, stdout, exit_status] = &rmcp_result.content[..] else {
+            panic!("expected 3 content items: {rmcp_result:?}");
+        };
+
+        assert_eq!(
+            cmd_line.as_text().unwrap().text,
+            "Executed command: `test_command --option`"
+        );
+        assert_eq!(stdout.as_text().unwrap().text, "This is a test output");
+        assert_eq!(exit_status.as_text().unwrap().text, "✅ test_tool: Success");
+    }
+}
