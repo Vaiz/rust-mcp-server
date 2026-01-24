bc055e3b0e416f80aa0e790873ff218224b6c496
Evgenii <vaiz.git@gmail.com>
2026-01-24 12:53:37 +0100

    chore: bump rmcp from 0.12.0 to 0.14.0 (#87)

--- build.rs
+++ build.rs
@@ -15,4 +15,13 @@ fn main() {
     } else {
         eprintln!("Failed to execute git command");
     }
+
+    let manifest = include_str!(env!("CARGO_MANIFEST_PATH"));
+    let version = manifest
+        .lines()
+        .find(|l| l.starts_with("rmcp = { version = \""))
+        .and_then(|l| l.strip_prefix("rmcp = { version = \""))
+        .and_then(|s| s.split_once('"').map(|(version, _)| version))
+        .unwrap_or("unknown");
+    println!("cargo:rustc-env=RMCP_VERSION={version}");
 }
